c0e4d2eef523906f20d6146cc6fb7c5dd6fa9213
les29rq <leo.strobel@uni-wuerzburg.de>
2025-07-01 14:12:11 +0200

    Load texture with buffer to enable loading from uber-jar

--- src/main/kotlin/graphic/Texture.kt
+++ src/main/kotlin/graphic/Texture.kt
@@ -1,11 +1,13 @@
 package de.uniwuerzburg.omodvisualizer.graphic
 
+import org.lwjgl.BufferUtils
 import org.lwjgl.opengl.GL20.*
 import org.lwjgl.opengl.GL30.glGenerateMipmap
 import org.lwjgl.stb.STBImage.*
 import org.lwjgl.system.MemoryStack.stackPush
 import java.nio.ByteBuffer
 import java.nio.IntBuffer
+import java.nio.channels.Channels
 
 class Texture {
     companion object {
@@ -14,13 +16,21 @@ class Texture {
             var width = 0
             var height = 0
             var image: ByteBuffer? = null
-            val fn = this::class.java.classLoader.getResource(resource)?.path.toString().drop(1)
+
+            // Load asset
+            val res = this::class.java.classLoader.getResource(resource)?.openStream()!!
+            val rBytes = res.available()
+            val buffer = BufferUtils.createByteBuffer(rBytes)
+            val channel = Channels.newChannel(res)
+            channel.read(buffer)
+            buffer.rewind()
+
             stackPush().use { stack ->
                 val w: IntBuffer = stack.mallocInt(1)
                 val h: IntBuffer = stack.mallocInt(1)
                 val comp: IntBuffer = stack.mallocInt(1)
                 stbi_set_flip_vertically_on_load(true)
-                image = stbi_load(fn, w, h, comp, 4)
+                image = stbi_load_from_memory(buffer, w, h, comp, 4)
                     ?: throw java.lang.RuntimeException((("Failed to load a texture file!"
                             + System.lineSeparator() + stbi_failure_reason())))
 
